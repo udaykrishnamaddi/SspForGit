af5673dc668ac23bf310012a2364a9b5332b9577
bhanu-teja2 <chbhanuteja@gmail.com>
2024-01-19 10:53:23 +0530

    loginchange1

--- app/Login/page.js
+++ app/Login/page.js
@@ -59,7 +59,7 @@ const Login = () => {
                             <button
                                 className="mt-5 tracking-wide font-semibold bg-green-400 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                                   <span className="ml-">
-                                      Log In
+                                      Sign In to SSP
                                   </span>
                             </button>
                             </Link>
